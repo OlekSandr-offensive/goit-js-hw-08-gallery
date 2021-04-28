8aa63fe98d8fbbd9caecc9f72caf241d46dcc11d
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-04-28 23:03:20 +0200

    додав функцю

--- create-gallery.js
+++ create-gallery.js
@@ -1,9 +1,11 @@
 import collection from './gallery-items.js';
 
 const container = document.querySelector('.js-gallery');
+// const urlRef = document.querySelector('');
 const cardMarkup = createGalleryHtml(collection);
 
 container.insertAdjacentHTML('afterbegin', cardMarkup);
+container.addEventListener('click', onContainerClick);
 
 function createGalleryHtml(collection) {
   return collection
@@ -20,8 +22,28 @@ function createGalleryHtml(collection) {
       alt="${description}"
     />
   </a>
-</li>;`;
+</li>`;
     })
     .join('');
 }
-console.log(createGalleryHtml(collection));
+// console.log(createGalleryHtml(collection));
+function onContainerClick(evt) {
+  const isGalleryLinkEl = evt.target.classList.contains('gallery__link');
+  if (!isGalleryLinkEl) {
+    return;
+  }
+  console.log(evt.target);
+}
+
+// const refs = {
+//   //   openModalBtn: document.querySelector('[data-modal-open]'),
+//   closeModalBtn: document.querySelector('[close-lightbox]'),
+//   //   backdrop: document.querySelector('[data-backdrop]'),
+// };
+
+// // refs.openModalBtn.addEventListener('click', toggleModal);
+// refs.closeModalBtn.addEventListener('click', toggleModal);
+
+// function toggleModal() {
+//   refs.backdrop.classList.toggle('is-open');
+// }
